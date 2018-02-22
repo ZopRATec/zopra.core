86e21e9257a2c267b566735c6c344f34dd3b41d1
Peter Seifert <peter.seifert1@tu-dresden.de>
2018-02-22 18:46:30 +0100

    #ZOPRA-166: add more checks to the value checker that handles the database values in getEntry to retain 0 values (that were converted to None before)

--- zopra/core/tables/Table.py
+++ zopra/core/tables/Table.py
@@ -338,11 +338,18 @@ class Table(SimpleItem, PropertyManager):
             # value handling speedup (1ms per call) using izip and the dict constructor that works on a list of 2-tuples and map (for checking each value)
             # the complex expression works as follows: 1) check if y is some kind of DateTime/datetime object, then use the strftime method to convert it into a string
             #                                          2) otherwise use the original value, if it is not empty
-            #                                          3) empty values are expressed as empty strings if the field is not of type singlelist
-            #                                          4) empty singlelist fields are expressed as None (this part could not be achieved by boolean expressions, but by inner if)
-            check = lambda (x, y): (x, hasattr(y, 'strftime') and y.strftime('%d.%m.%Y') or y or ('' if (y is None and self.getField(x)[ZC.COL_TYPE] != 'singlelist') else None))
+            #                                          3) None values are expressed as empty strings if field is not of type singlelist
+            #                                          4) value of 0 is expressed as 0
+            #                                          5) None-valued singlelist fields are expressed as None 
+            #                                          6) everything else (other false values) is expressed as None
+            # 3 to 6 could not be achieved by boolean expressions, but by inner if
+            if self.tablename == 'fakultaet':
+                print result
+                print cols_list
+            check = lambda (x, y): (x, hasattr(y, 'strftime') and y.strftime('%d.%m.%Y') or y or ('' if (y is None and self.getField(x)[ZC.COL_TYPE] != 'singlelist') else (0 if y == 0 else None)))
             entry = dict(map(check, izip(cols_list, result)))
-
+            if self.tablename == 'fakultaet':
+                print map(check, izip(cols_list, result))
             # add multilist ids
             autoid = entry[TCN_AUTOID]
             # get all multilists for the table
