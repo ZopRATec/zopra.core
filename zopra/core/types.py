9ada47622bfba1579db23543a4573f331019038a
Klaus Ramm <klaus.ramm@tu-dresden.de>
2019-06-19 11:41:30 +0200

    ZOPRA-290: Add safty swicht for python 3 typehandling
    - Remove all trailing whitespaces

--- zopra/core/types.py
+++ zopra/core/types.py
@@ -3,14 +3,16 @@ Created on 6 May 2018
 
 @author: Peter Seifert
 '''
-# is this necessary?
-#from builtins import int, str
 
+# Python 3 safety switch:
+try:
+    UnicodeType = unicode
+except:
+    UnicodeType = str
 IntType     = int
 LongType    = int
 ListType    = list
 StringType  = str
-UnicodeType = str
 TupleType   = tuple
 BooleanType = bool
 FloatType   = float
