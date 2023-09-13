3ed52dd3abdf62b22ffe794d34a2f705bbd9fb94
Peter Seifert <peter.seifert1@tu-dresden.de>
2023-09-13 12:30:31 +0200

    WEBCMS-4554 move button handler for table search result into template base manager
    Some additional classes (in Table.py, TableNode.py) do not have security declarations, restricting what can be done in skin-scripts.
    In preparation of the bugfix, the code needs to move into the manager.

--- zopra/core/tools/TemplateBaseManager.py
+++ zopra/core/tools/TemplateBaseManager.py
@@ -936,3 +936,38 @@ class TemplateBaseManager(Manager):
             if not key.endswith("_AND"):
                 return True
         return False
+
+    def zopra_table_search_result_handler(self, table, columns, order, cons_key=[]):
+        """Handle search result listing buttons."""
+        request = self.REQUEST
+        buttons = self.getGenericConfig(table).get("ownButtonActions")
+
+        # preparation for when we have checkboxes in the list again
+        autoids = request.get("checked_ids")
+        if not autoids:
+            # no selection -> rebuild constraints from the lists cons_key and corresponding value fields
+            cons = {}
+            for key in cons_key:
+                cons[key] = request.get(key + "_values")
+                # TODO: check if this is necessary and make it go away
+                if cons[key] == "False":  # converting string form values
+                    cons[key] = 0
+                if cons[key] == "True":
+                    cons[key] = 1
+            # TODO: this might not work for subtable constraints -> make it work (ERP Positions)
+            autoids = self.tableHandler[table].getEntryAutoidList(constraints=cons, order=order)
+
+        if request.get("form.button.Export"):
+            return self.zopra_table_search_result_export_csv(
+                table, columns, autoids, request
+            )
+        else:
+            for button in buttons:
+                if request.get(button["id"]):
+                    # found the pressed button
+                    target = button.get("target")
+                    target_func = getattr(self, target)
+                    # call target function with table, list of selected columns, current order column and list of constraint keys (with corresponding values in the request)
+                    return target_func(table, columns, autoids, request)
+        # nothing found
+        return "No target found. Something wrong."
